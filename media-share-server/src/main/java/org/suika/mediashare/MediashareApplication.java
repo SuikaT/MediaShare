4967c724738df58d55f7e2d2ada5b8acdc66e892
SuikaT <bourney.julien@gmail.com>
2024-01-01 02:25:35 +0100

    create back end project

---
+++ media-share-server/src/main/java/org/suika/mediashare/MediashareApplication.java
@@ -0,0 +1,13 @@
+package org.suika.mediashare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediashareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediashareApplication.class, args);
+	}
+
+}
---
+++ media-share-server/src/test/java/org/suika/mediashare/MediashareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.suika.mediashare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediashareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
