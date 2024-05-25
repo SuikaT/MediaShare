4f56cff7a215d8ff94c67bc2355458283a8c0188
SuikaT <bourney.julien@gmail.com>
2024-05-26 01:28:20 +0200

    Setup run app task

--- media-share-server/src/main/java/org/suika/mediashare/controller/MediaController.java
+++ media-share-server/src/main/java/org/suika/mediashare/controller/MediaController.java
@@ -1,18 +1,13 @@
 package org.suika.mediashare.controller;
 
-import java.io.File;
 import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import org.suika.mediashare.model.classes.Media;
 import org.suika.mediashare.model.classes.MediaFile;
 import org.suika.mediashare.model.enums.MediaTypeEnum;
