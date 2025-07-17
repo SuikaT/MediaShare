f0abf4fe90b9100852873f80db7fcb825192813d
SuikaT <bourney.julien@gmail.com>
2025-07-18 00:08:19 +0200

    Fix ip

--- media-share-server/src/main/java/org/suika/mediashare/configuration/WebConfig.java
+++ media-share-server/src/main/java/org/suika/mediashare/configuration/WebConfig.java
@@ -12,19 +12,19 @@ import org.suika.mediashare.ApplicationProperties;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    ApplicationProperties appProperties;
+	@Autowired
+	ApplicationProperties appProperties;
 
-    @Bean
-    public WebMvcConfigurer corsConfig() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                String origins = "http://" + appProperties.getIp() + ":" + appProperties.getClientPort();
-                registry.addMapping("/**").allowedOrigins(origins).allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name()).allowedHeaders(HttpHeaders.CONTENT_TYPE,
-                        HttpHeaders.AUTHORIZATION);
-            }
-        };
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				String origins = "http://" + appProperties.getIp() + ":" + appProperties.getClientPort();
+				registry.addMapping("/**").allowedOrigins(origins).allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name()).allowedHeaders(HttpHeaders.CONTENT_TYPE,
+						HttpHeaders.AUTHORIZATION);
+			}
+		};
 
-    }
+	}
 }
